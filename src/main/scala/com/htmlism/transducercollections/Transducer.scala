159f32fd56da4e8397f36c9707035dbe24d88a84
Mark Canlas <github@htmlism.com>
2015-07-12 09:58:57 -0400

    g to red

--- src/main/scala/com/htmlism/transducercollections/Transducer.scala
+++ src/main/scala/com/htmlism/transducercollections/Transducer.scala
@@ -1,34 +1,34 @@
 package com.htmlism.transducercollections
 
 trait Transducer[A, B] {
-  def apply[R](f: Reducer[R, B]): Reducer[R, A]
+  def apply[R](red: Reducer[R, B]): Reducer[R, A]
 
-  def foldUsing[R](f: Reducer[R, B]): Reducer[R, A] = apply[R](f)
+  def foldUsing[R](red: Reducer[R, B]): Reducer[R, A] = apply[R](red)
 
   def andThen[C](that: Transducer[B, C]): Transducer[A, C] = new Composite(this, that)
 }
 
 class Composite[A, B, C](left: Transducer[A, B], right: Transducer[B, C]) extends Transducer[A, C] {
-  def apply[R](f: Reducer[R, C]) = left(right(f))
+  def apply[R](red: Reducer[R, C]) = left(right(red))
 }
 
 class Filter[A](f: A => Boolean) extends Transducer[A, A] {
-  def apply[R](g: Reducer[R, A]) =
+  def apply[R](red: Reducer[R, A]) =
     (r, a) =>
       if (f(a))
-        g(r, a)
+        red(r, a)
       else
         r
 }
 
 class Mapper[A, B](f: A => B) extends Transducer[A, B]  {
-  def apply[R](g: Reducer[R, B]) =
-    (r, a) => g(r, f(a))
+  def apply[R](red: Reducer[R, B]) =
+    (r, a) => red(r, f(a))
 }
 
 class FlatMapper[A, B](f: A => TraversableOnce[B]) extends Transducer[A, B]  {
-  def apply[R](g: Reducer[R, B]) =
-    (r, a) => f(a).foldLeft(r)(g)
+  def apply[R](red: Reducer[R, B]) =
+    (r, a) => f(a).foldLeft(r)(red)
 }
 
 object Filter {
