f26850d86c82285cb7112515380223c4c9ff0e35
Mark Canlas <github@htmlism.com>
2016-03-05 11:13:21 -0500

    added doc

--- src/main/scala/com/htmlism/transducercollections/package.scala
+++ src/main/scala/com/htmlism/transducercollections/package.scala
@@ -12,6 +12,15 @@ package object transducercollections {
 
   type Reducer[X, A] = (X, A) => X
 
+  /**
+    * The concept of transducers boiled down to a single type alias.
+    *
+    * Not consumed anywhere in this code base.
+    *
+    * @tparam A
+    * @tparam B
+    */
+
   type TransducerType[A, B] = (Reducer[X, B] => Reducer[X, A]) forSome { type X }
 }
 
